be1880c04fd390a5793d096917ffe1cfa0ca99a1
VooDoo <dearvoodoo.yt@gmail.com>
2020-09-08 17:37:25 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿﻿$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
