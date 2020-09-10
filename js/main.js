6899bb601500678d7c69d85202e996eb9f473ca2
VooDoo <dearvoodoo.yt@gmail.com>
2020-09-10 05:43:03 +0200

    1.2.0

--- js/main.js
+++ js/main.js
@@ -6,3 +6,63 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+jQuery(function ($) {
+    var container = $(".imgsBA");
+    var iBefore = $(".imgsBA #before");
+
+    function comparePos(e) {
+        e.preventDefault();
+        var mLeft = e.pageX ? e.pageX : e.touches[0].pageX;
+        var cLeft = container.offset().left;
+        var w = container.width();
+        var pos = (mLeft - cLeft) / w * 100;
+        iBefore.css({
+            width: pos + "%"
+        });
+    }
+
+    container.on('mousemove', comparePos);
+    container.on('touchstart', comparePos);
+    container.on('touchmove', comparePos);
+});
+
+jQuery(function ($) {
+    var container = $(".imgsDC");
+    var iBefore = $(".imgsDC #before");
+
+    function comparePos(e) {
+        e.preventDefault();
+        var mLeft = e.pageX ? e.pageX : e.touches[0].pageX;
+        var cLeft = container.offset().left;
+        var w = container.width();
+        var pos = (mLeft - cLeft) / w * 100;
+        iBefore.css({
+            width: pos + "%"
+        });
+    }
+
+    container.on('mousemove', comparePos);
+    container.on('touchstart', comparePos);
+    container.on('touchmove', comparePos);
+});
+
+jQuery(function ($) {
+    var container = $(".imgsFE");
+    var iBefore = $(".imgsFE #before");
+
+    function comparePos(e) {
+        e.preventDefault();
+        var mLeft = e.pageX ? e.pageX : e.touches[0].pageX;
+        var cLeft = container.offset().left;
+        var w = container.width();
+        var pos = (mLeft - cLeft) / w * 100;
+        iBefore.css({
+            width: pos + "%"
+        });
+    }
+
+    container.on('mousemove', comparePos);
+    container.on('touchstart', comparePos);
+    container.on('touchmove', comparePos);
+});
